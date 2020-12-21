b529ab3ad5780169b9da535920c8b6b58290aeb2
Daniel Austi <danielausti@gmail.com>
2020-12-20 16:10:59 -0800

    Add procfile, update ports, update .gitignore

--- app.js
+++ app.js
@@ -125,8 +125,13 @@ if (listName === "Today") {
   }
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
 // testing server
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+app.listen(port, function(){
+    console.log("Server has started successfully.");
 });
 
